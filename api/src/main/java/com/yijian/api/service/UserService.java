a6364d7a6dd7bbff9b7b005b1e6ec1c13d5ff160
zmjiangi <zmjiangi@qq.com>
2018-06-07 14:50:36 +0800

    服务降级

--- api/src/main/java/com/yijian/api/service/UserService.java
+++ api/src/main/java/com/yijian/api/service/UserService.java
@@ -1,13 +1,17 @@
 package com.yijian.api.service;
 
+import com.yijian.api.fallback.UserServiceFallback;
 import com.yijian.api.model.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by zmjiangi on 2018/5/28.
  */
-@FeignClient(name = "provider-service")
+@FeignClient(name = "provider-service", fallback = UserServiceFallback.class)
 public interface UserService {
 
     @GetMapping(value = "/svc/user/getName")
